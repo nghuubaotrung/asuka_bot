e388c28faf74d8da3f81c7ba2b9d27fdc1e1f03f
Nguyen Huu Bao Trung <nguyen.huubaotrung@adways.net>
2016-05-29 18:54:27 +0900

    added exception handling

--- script/rundeck_script/update_list.sh
+++ script/rundeck_script/update_list.sh
@@ -5,9 +5,8 @@ PUBLISH_CHANNEL="#release"
 TEST_CHANNEL="test"
 
 ## asuka用
-WEBHOOK_URL="https://hooks.slack.com/services/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"
+WEBHOOK_URL=xxxxxxxxxxxxxxxxxxxxxxxxx
 
-## asuka-monitor WebHook : https://hooks.slack.com/services/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
 
 ## 日付取得
 CHECK_DATE="$(date -v+4m +'%Y-%m-%d')"
@@ -28,13 +27,16 @@ CHECK_DAY="$(date -v+4m +'%d')"
 DAY_BEFORE="$(date -v+4m -v-1d +'%Y-%m-%d')"
 
 #本番
-HOUSE_LIST=("11398837" "11033998" "11574448")
+FULL_HOUSE="11398837"
+CONY="11033998"
+BROWN="11574448"
+
+HOUSE_LIST=($FULL_HOUSE $CONY $BROWN)
 
 #sandbox用
 #HOUSE_LIST=("7447209")
 
-
-echo $AIRBNB_TOKEN
+RESPONSE=200
 
 for house in "${HOUSE_LIST[@]}"
 do
@@ -43,28 +45,35 @@ do
 
     # update Calendar
     # block The Day Before
-    curl -i -X PUT --compressed -H "X-Airbnb-OAuth-Token: $AIRBNB_TOKEN" \
+    RESPONSE=$(curl -i -X PUT --compressed -H "X-Airbnb-OAuth-Token: $AIRBNB_TOKEN" \
         -H "Content-Type: application/json; charset=UTF-8" \
         --data-binary '{"availability":"unavailable"}' \
-        https://api.airbnb.com/v2/calendars/$house/$DAY_BEFORE/$DAY_BEFORE
+        https://api.airbnb.com/v2/calendars/$house/$DAY_BEFORE/$DAY_BEFORE \
+        2>/dev/null | head -n 1 | cut -d$' ' -f2)
 
     # open Check Day
-    curl -i -X PUT --compressed -H "X-Airbnb-OAuth-Token: $AIRBNB_TOKEN" \
+    RESPONSE=$(curl -i -X PUT --compressed -H "X-Airbnb-OAuth-Token: $AIRBNB_TOKEN" \
         -H "Content-Type: application/json; charset=UTF-8" \
         --data-binary '{"availability":"available"}' \
-        https://api.airbnb.com/v2/calendars/$house/$CHECK_DATE/$CHECK_DATE
+        https://api.airbnb.com/v2/calendars/$house/$CHECK_DATE/$CHECK_DATE \
+        2>/dev/null | head -n 1 | cut -d$' ' -f2)
+
 
     # update Get Around
     if [[ $(($CHECK_DAY % 2)) -eq 0 ]]; then
+        echo "update list"
         curl -i -X POST --compressed -H "X-Airbnb-OAuth-Token: $AIRBNB_TOKEN" \
             -H "Content-Type: application/json; charset=UTF-8" \
             --data-binary '{"listing":{"transit":"4 minutes train ride to Shinjuku station. (direct) \n4 minutes train ride to Ikebukuro station. (direct) \n9 minutes train ride to Harajuku station. (direct) \n11 minutes train ride to Shibuya station. (direct)"}}' \
-            https://api.airbnb.com/v1/listings/$house/update?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD
+            https://api.airbnb.com/v1/listings/$house/update?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD \
+            2>/dev/null | head -n 1 | cut -d$' ' -f2
     else
+        echo "update list"
         curl -i -X POST --compressed -H "X-Airbnb-OAuth-Token: $AIRBNB_TOKEN" \
             -H "Content-Type: application/json; charset=UTF-8" \
             --data-binary '{"listing":{"transit":"4 minutes train ride to Shinjuku station. (Direct) \n4 minutes train ride to Ikebukuro station. (Direct) \n9 minutes train ride to Harajuku station. (Direct) \n11 minutes train ride to Shibuya station. (Direct)"}}' \
-            https://api.airbnb.com/v1/listings/$house/update?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD
+            https://api.airbnb.com/v1/listings/$house/update?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD \
+            2>/dev/null | head -n 1 | cut -d$' ' -f2
     fi
 
     # add Photo
@@ -74,6 +83,8 @@ do
         #                -F filename="~/Source-code/asuka_bot/script/rundeck_script/test_profile.png" \
         #                https://api.airbnb.com/v1/listings/$house/update?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD
 done
+
+echo $RESPONSE
 
 
 ##### 通知
@@ -85,7 +96,7 @@ publish_data=`cat << EOF
         "attachments": [{
             "color": "#FACC2E",
             "title": "RELEASE REPORT @here" ,
-            "text": "各リストを更新しました。更新内容は以下の通りです。\n\n1. Blocked: $DAY_BEFORE\n2. Opened: $CHECK_DATE\n3. Updated: Get Around\n\n 以上です。Binさん、Junさん、ご確認宜しくお願い致します。\n\n\n https://www.airbnb.com/rooms"
+            "text": "各リストを更新しました。更新内容は以下の通りです。\n\n1. Blocked: $DAY_BEFORE\n2. Opened: $CHECK_DATE\n3. Updated: Get Around\n\n Request Response Code: $RESPONSE \n\n 以上です。Binさん、Junさん、ご確認宜しくお願い致します。\n\n\n https://www.airbnb.com/rooms"
 
         }]
     }
@@ -93,3 +104,7 @@ EOF`
 
 ## 通知送る
 curl -X POST --data-urlencode "$publish_data" $WEBHOOK_URL
+
+if [ $RESPONSE -ne 200 ]; then
+    exit 1;
+fi
