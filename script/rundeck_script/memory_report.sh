545e1df70c2394f0f272549591a0138555cefe83
Nguyen Huu Bao Trung <nguyen.huubaotrung@adways.net>
2016-05-31 13:54:21 +0900

    update server monitoring server

--- script/rundeck_script/memory_report.sh
+++ script/rundeck_script/memory_report.sh
@@ -2,12 +2,14 @@
 
 NAME="asuka"
 PUBLISH_CHANNEL="#monitor"
-TEST_CHANNEL="test"
 
-## asuka用
-WEBHOOK_URL=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+### test
+#PUBLISH_CHANNEL="#test"
+
+WEBHOOK_URL=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 MEMORY_REPORT=$(top -l 1 -s 0 | grep PhysMem | sed 's/, /         /g')
+SLEEP_STATUS=$(sudo systemsetup -getcomputersleep)
 
 ##### 通知
 publish_data=`cat << EOF
@@ -17,8 +19,8 @@ publish_data=`cat << EOF
         "link_names": 1,
         "attachments": [{
             "color": "#FACC2E",
-            "title": "Rundeck Server Monitoring" ,
-            "text": "Testing Server in Sleep Mode \n\n $MEMORY_REPORT"
+            "title": "Asuka's Rundeck Server Monitoring" ,
+            "text": "1. Memory Check: \n\n  - $MEMORY_REPORT \n\n 2. Idle Status: \n\n - $SLEEP_STATUS"
         }]
     }
 EOF`
