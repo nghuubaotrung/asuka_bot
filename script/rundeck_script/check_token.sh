11834d18d2d9f3e2c85be2dd5f156434a0d91baa
Nguyen Huu Bao Trung <nguyen.huubaotrung@adways.net>
2016-06-09 14:42:58 +0900

    updated scripts

--- script/rundeck_script/check_token.sh
+++ script/rundeck_script/check_token.sh
@@ -5,7 +5,7 @@ AIRBNB_TOKEN=$(curl -X POST -d "client_id=3092nxybyb0otqw18e8nh5nty" \
     -d "locale=en-US" \
     -d "currency=USD" \
     -d grant_type=password \
-    -d password=xxxxxxxxxxxxx \
+    -d password=xxxxxxxxxxxxxxxx \
     -d username=tokiokichi202@gmail.com https://api.airbnb.com/v1/authorize | sed -e 's/[{}]/''/g' | cut -d , -f 1 | sed 's/.*\://' | sed 's/[^"]*"\([^"]*\)".*/\1/')
 
 echo $AIRBNB_TOKEN
---
+++ script/rundeck_script/search_list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME="asuka"
+PUBLISH_CHANNEL="#release"
+TEST_CHANNEL="test"
+
+## asuka用
+#WEBHOOK_URL=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+CHECK_MONTH="$(date -v+4m +'%m')"
+CHECK_YEAR="$(date -v+4m +'%Y')"
+
+LAST_DAY="$(cal $CHECK_MONTH $CHECK_YEAR | egrep -e '^ [0-9]|^[0-9]' | tr '\n' ' ' | awk '{print $NF}')"
+
+FIRST_DATE=$CHECK_YEAR-$CHECK_MONTH-01
+LAST_DATE=$CHECK_YEAR-$CHECK_MONTH-$LAST_DAY
+
+LOCATION="Tokyo"
+
+echo $FIRST_DATE
+echo $LAST_DATE
+
+curl https://api.airbnb.com/v2/search_results?client_id=3092nxybyb0otqw18e8nh5nty&locale=en-US&currency=USD&_format=for_search_results_with_minimal_pricing&_limit=10&_offset=0&fetch_facets=true&guests=1&ib=false&ib_add_photo_flow=true&location=Lake%20Tahoe%2C%20CA%2C%20US&min_bathrooms=0&min_bedrooms=0&min_beds=1&min_num_pic_urls=10&price_max=210&price_min=40&sort=1&user_lat=37.3398634&user_lng=-122.0455164
